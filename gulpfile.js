5aec0a1e1244d291a69a19dfc19030854134a796
youngsoo shin <sys920@gmail.com>
2018-12-10 09:48:30 -0600

    Adding gulp-cleanCSS for css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
+let cleanCSS = require('gulp-clean-css');
 
-gulp.task('autoprefixer', () => {
+gulp.task('css', () => {
 	return gulp.src('./src/**/*.css') 
 		.pipe(autoprefixer({
         browsers: ['last 2 versions']
     }))
+    .pipe(cleanCSS())
 		.pipe(gulp.dest('./dist/'))
 });
