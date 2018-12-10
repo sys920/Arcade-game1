9ae09bc23b0448b501fde0599a299a0c8dd96959
youngsoo shin <sys920@gmail.com>
2018-12-10 09:54:33 -0600

    Adding gulp-imagemin for images

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 let cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
 
 gulp.task('css', () => {
 	return gulp.src('./src/**/*.css') 
@@ -9,4 +10,10 @@ gulp.task('css', () => {
     }))
     .pipe(cleanCSS())
 		.pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('images', () => {
+  return gulp.src('./src/images/**/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('./dist/images'))
 });
