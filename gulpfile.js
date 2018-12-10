0eb75f261b434ef453d991c81b90831ba864526f
youngsoo shin <sys920@gmail.com>
2018-12-10 10:01:40 -0600

    Adding gulp-babel to transpile ths JS

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 let cleanCSS = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
+const babel = require('gulp-babel');
 
 gulp.task('css', () => {
 	return gulp.src('./src/**/*.css') 
@@ -16,4 +17,12 @@ gulp.task('images', () => {
   return gulp.src('./src/images/**/*')
   .pipe(imagemin())
   .pipe(gulp.dest('./dist/images'))
+});
+
+gulp.task('js', () => {
+  return gulp.src('./src/js/**/*.js')
+  .pipe(babel({
+      presets: ['@babel/env']
+  }))  
+  .pipe(gulp.dest('./dist/js'))
 });
